35dda3c80116c6f2a6919174eb6550008ac978f6
Praveen Chaurasia <108619467+0xpraveen@users.noreply.github.com>
2023-06-17 19:44:30 +0530

    Create BitwiseOperatorExample.java

---
+++ Code/BitwiseOperatorExample.java
@@ -0,0 +1,11 @@
+public class BitwiseOperatorExample
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Value of 5|6:"+(5|6));
+		System.out.println("Value of 5&6:"+(5&6));
+		System.out.println("Value of 5>>6:"+(5>>6));
+		System.out.println("Value of 5<<6:"+(5<<6));
+	}
+}
