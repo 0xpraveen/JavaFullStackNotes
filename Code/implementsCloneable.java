c2a9704910f81f03b665cbf663ecc3f1b1ce7d84
Praveen Chaurasia <108619467+0xpraveen@users.noreply.github.com>
2023-06-18 00:14:10 +0530

    Update implementsCloneable.java

--- Code/implementsCloneable.java
+++ Code/implementsCloneable.java
@@ -25,3 +25,33 @@ class Student implementsCloneable
 	}
 	
 }
+public class CopyConstructorExample 
+{
+	
+	public static void main(String[] args)throws CloneNotSupportedException
+	{
+		Student obj1=new Student("Tamanna",101);
+		Student obj2=new Student(obj1);
+
+		Student obj3=new Student();
+		obj3.name=obj2.name;
+		obj3.roll=obj2.roll;
+
+		Student obj4=new Student();
+		obj4=(Student)obj2.clone();
+
+		System.out.println("Obj1 = obj1.name :"+obj1.name);
+		System.out.println("Obj1 = obj1.roll :"+obj1.roll);
+		
+		System.out.println("Obj2 = obj2.name :"+obj2.name);
+		System.out.println("Obj2 = obj2.roll :"+obj2.roll);
+
+		System.out.println("Obj3 = obj3.name :"+obj3.name);
+		System.out.println("Obj3 = obj3.roll :"+obj3.roll);
+
+		System.out.println("Obj4 = obj4.name :"+obj4.name);
+		System.out.println("Obj4 = obj4.roll :"+obj4.roll);
+		
+	}
+}
+
