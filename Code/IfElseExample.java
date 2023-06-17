3464f2b32d87988cf443b4f878cec15e6cb41dfd
Praveen Chaurasia <108619467+0xpraveen@users.noreply.github.com>
2023-06-17 19:50:10 +0530

    Create IfElseExample.java

---
+++ Code/IfElseExample.java
@@ -0,0 +1,9 @@
+public class IfElseExample
+{
+	public static void main(String[] args)
+	{
+		int num=5;
+		if(num%2==0);//';'this sign close the if condition thats why Even will print always;
+		System.out.println("Even");
+	}
+}
