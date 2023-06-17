9da4adbfb845ed6a72300a375a7794430937e13e
Praveen Chaurasia <108619467+0xpraveen@users.noreply.github.com>
2023-06-17 19:46:02 +0530

    Create AdditionProgramWithoutArithmaticOperator.java

---
+++ Problems/AdditionProgramWithoutArithmaticOperator.java
@@ -0,0 +1,19 @@
+public class AdditionProgramWithoutArithmaticOperator
+{
+	public int gerResult(int num1,int num2)
+	{
+		while(num2!=0
+		{
+			int carry=(num1&num2);
+			num1=(num1^num2);
+			num2=(carry<<1);
+		}
+		return num1;
+	}
+	public static void main(String[] args)
+	{
+		AdditionProgramWithoutArithmaticOperator apwao= new AdditionProgramWithoutArithmaticOperator();
+		int res=apwao.getResult(7,5);
+		System.out.println("The Addition of Two numbers :"+res);
+	}
+}
