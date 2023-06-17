6920d6393eedd489c307afd7b7680c7b5ed66c36
Praveen Chaurasia <108619467+0xpraveen@users.noreply.github.com>
2023-06-17 19:52:05 +0530

    Create SwicthCaseExample.java

---
+++ Code/SwicthCaseExample.java
@@ -0,0 +1,17 @@
+public class SwicthCaseExample
+{
+	public static void main(String[] args)
+	{
+		char grade='E';
+		switch(grade)
+		{
+			default:System.out.println("invalid");break;
+			case 'A':System.out.println("A");break;
+			case 'B':System.out.println("B");break;
+			case 'C':System.out.println("C");break;
+			case 'D':System.out.println("D");
+			
+			
+		}
+	}
+}
